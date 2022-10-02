82d2a652716970692edb28ce27232dbf5507f9c7
Syeda Kainat Fatima <63343529+SyedaFatima525@users.noreply.github.com>
2022-10-02 20:17:47 +0500

    Add files via upload

--- Task_45.js
+++ Task_45.js
@@ -1,36 +1,30 @@
-// function carInfo(manufacturer, model,color,body ) {
-//     let object;
-//     return object={
-//        Manufacturer: manufacturer,
-//        Model:model,
-//        Color:color,
-//     Body:body
-//     }
-// }
 
-// console.log( carInfo("Acura",2019, "Red","SEDAN"));
-
-// console.log( carInfo("WagonR",2021,"Red"));
-
-
-
-function carInfo(manufacturer, model,...optionalData ) {
-  var object;
-    return object={
-       Manufacturer: manufacturer,
-       Model:model,
-       //var OptionalData : {optionalData};
-       =
-       function foo() {
-        for (var i = 0; i < optionalData.length; i++) {
-          console.log(optionalData[i]);
-      }
+class Car{
+  constructor(manufacturer,carModel,...optionalData){
+    this.manufacturer = manufacturer;
+    this.carModel = carModel;
+    this.optionalData = optionalData
     
+  }
+
+  showData(){
+    console.log(this.manufacturer)
+    
+    console.log(this.carModel)
+
+    for (var i = 0; i < this.optionalData.length; i++) {
+      console.log( this.optionalData[i]);
     }
+    console.log("\n")
+  }
 }
 
-console.log( carInfo("Acura",2019, {Color: "Red",body:"SEDAN"}));
 
-console.log( carInfo("WagonR",2021, {Color: "Red"}));
+var car = new Car(manufacturer="Suzki",carModel="Wagon R",color="white",reg="Lahore")
+car.showData()
+
+
+var car = new Car(manufacturer="Honda",carModel="City",color="Black",reg="Karachi",body="Sedan")
+car.showData()
 
 
